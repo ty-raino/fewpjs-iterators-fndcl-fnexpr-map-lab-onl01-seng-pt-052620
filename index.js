ffbc5228d390bb13a26177ccce752ec918b37b2a
github username <github email address>
2020-12-15 23:29:56 -0600

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  })
 }
+
+
+
